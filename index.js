1a2d2686d41304e971c6ff347b893eeebb08cfbe
Tom Agostino <agost.tv267@gmail.com>
2023-05-31 10:24:31 -0700

    first upload, node project

---
+++ index.js
@@ -0,0 +1,28 @@
+var http = require("http");
+var https = require("https");
+var url = require("url");
+var StringDecoder = require("string_decoder").StringDecoder;
+var config = require("./config");
+var fs = require("fs");
+var handlers = require("./lib/handlers");
+
+var httpServer = http.createServer(function (req, res) {
+  unifiedServer(req, res);
+});
+
+httpServer.listen(config.httpPort, function () {
+  console.log("The server is listening on port " + config.httpPort);
+});
+
+var httpsServerOptions = {
+  key: fs.readFileSync("./https/key.pem"),
+  cert: fs.readFileSync("./https/cert.pem"),
+};
+
+var httpsServer = https.createServer(httpsServerOptions, function (req, res) {
+  unifiedServer(req, res);
+});
+
+httpsServer.listen(config.httpsPort, function () {
+  console.log("The server is listening on port " + config.httpsPort);
+});
